da86b2f2c9f7a05c407a2522e6266bc775993f2d
ZiyanShabbir204 <ziyanwehvaria@gmail.com>
2024-12-19 23:44:12 +0500

    server updated

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
 app.use(cookieParser());
-const whiteList = ["http://localhost:5173", "http://localhost:5174"];
+const whiteList = ["http://localhost:5173", "http://localhost:5174","https://urgefragrances.com"];
 const corsOptions = {
   credentials: true,
   origin: whiteList,
